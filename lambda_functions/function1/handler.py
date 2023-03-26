f44de98657abe56b6cb6fcf40ecfb1dd2648354b
aryan jain <arjain@deqode.com>
2023-03-27 00:56:45 +0530

    update f1 handler

--- lambda_functions/function1/handler.py
+++ lambda_functions/function1/handler.py
@@ -2,6 +2,6 @@ def lambda_handler(event, context):
     print("Hello from function")
 
     print("changed file")
-    print("chnaged file2")
+    # print("chnaged file2")
     # print("chnaged file3")
     print("chnaged file4")
